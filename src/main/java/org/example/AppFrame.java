8f8a51e75863ee4e1e357541c953a31290ee538c
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-09-07 00:35:27 +0300

    Initial commit

---
+++ src/main/java/org/example/AppFrame.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import javax.swing.*;
+
+public class AppFrame extends JFrame {
+    JLabel titleLabel;
+    JSlider slider;
+    JTextField threadTextField1;
+    JTextField threadTextField2;
+    JButton startThread1Btn;
+    JButton startThread2Btn;
+
+    public AppFrame() {
+        super("Lab1");
+    }
+
+}
