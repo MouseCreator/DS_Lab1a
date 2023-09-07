16e396a1d473e78da382e00f301fbb484533b1ff
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-09-08 01:12:00 +0300

    Added thread delay

--- src/main/java/org/example/SliderMoveThreadFactory.java
+++ src/main/java/org/example/SliderMoveThreadFactory.java
@@ -5,6 +5,7 @@ import java.util.function.Predicate;
 
 public class SliderMoveThreadFactory {
     private static final Object obj = new Object();
+    private static final int DELAY = 100;
     private Thread getThread(JSlider slider, Predicate<Integer> canMove, int delta, int priority) {
         Thread thread = new Thread(()->{
             while (true) {
@@ -12,6 +13,11 @@ public class SliderMoveThreadFactory {
                     int prevValue = slider.getValue();
                     if (canMove.test(prevValue)) {
                         slider.setValue(prevValue + delta);
+                    }
+                    try {
+                        Thread.sleep(DELAY);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
                     }
                 }
             }
